c2739e91f862bbef23e6c5aadbd61f218ec9c21f
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2021-05-16 20:11:34 +0200

    starter file

---
+++ tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Student(
+    id INT , PRIMARY KEY("id"),
+    first_name NVARCHAR2(50) ,
+    last_name NVARCHAR2(50) ,
+    date of birth DATE ,
+    email NVARCHAR2(255) ,
+    credit card INT,
+    gender NVARCHAR2(6),
+    phone NVARCHAR2(11) ,
+    levels INT ,
+    department_id INT 
+);
