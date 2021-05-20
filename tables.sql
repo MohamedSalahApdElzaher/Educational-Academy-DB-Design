b77cfccbcccaf3180f542c222645b06d80b46776
youssef magdy <47723356+youssefmagdy1@users.noreply.github.com>
2021-05-20 09:15:37 +0200

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -10,3 +10,28 @@ CREATE TABLE Student(
     levels INT ,
     department_id INT 
 );
+CREATE TABLE `Department` ( 
+    `Department_id` INT NOT NULL AUTO_INCREMENT , 
+    `Department_name` VARCHAR(255) NOT NULL ,
+    `Department_details` TEXT NOT NULL ,  
+    `Manager` VARCHAR(128) NOT NULL , 
+    PRIMARY KEY  (`Department_id`)
+);
+
+CREATE TABLE `Address` ( 
+    `User_id` INT NOT NULL AUTO_INCREMENT , 
+    `City` VARCHAR(128) NOT NULL ,
+    `Street` TEXT NOT NULL ,  
+    `State` VARCHAR(128)  NULL , 
+    PRIMARY KEY  (`User_id`)
+);
+
+
+CREATE TABLE `Rooms` ( 
+    `Room_id` INT NOT NULL AUTO_INCREMENT , 
+    `Name` INT NOT NULL AUTO_INCREMENT , 
+    `Size` VARCHAR(128) NOT NULL ,
+    `Number` TEXT NOT NULL ,  
+    `Floor` VARCHAR(128)  NULL , 
+    PRIMARY KEY  (`Room_id`)
+);
