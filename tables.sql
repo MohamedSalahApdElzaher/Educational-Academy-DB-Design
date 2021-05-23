eebdaeab2074f2ad7e1867dd253704061b4b01fa
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2021-05-24 01:28:53 +0200

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Student(
-    id INT , PRIMARY KEY("id"),
-    first_name NVARCHAR2(50) ,
-    last_name NVARCHAR2(50) ,
-    date of birth DATE ,
-    email NVARCHAR2(255) ,
-    credit card INT,
-    gender NVARCHAR2(6),
-    phone NVARCHAR2(11) ,
-    levels INT ,
-    department_id INT 
+    "id" INT , PRIMARY KEY("id"),
+    "first_name" NVARCHAR2(50) ,
+    "last_name" NVARCHAR2(50) ,
+    "date of birth" DATE ,
+    "email" NVARCHAR2(255) ,
+    "credit card" INT,
+    "gender" NVARCHAR2(6),
+    "phone" NVARCHAR2(11) ,
+    "levels" INT ,
+    constraint fk_student_department foreign key ("department_id")
 );
 CREATE TABLE `Department` ( 
     `Department_id` INT NOT NULL AUTO_INCREMENT , 
