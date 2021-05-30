2c3e1f6643430c3dbd8a1c0435de8dc540d4b048
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2021-05-30 13:11:07 +0200

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,28 @@
+INSERT All 
+
+INTO Student 
+(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+VALUES
+('muhammad', 'salah', '01-2-1999', 'mosala7@mail.com', 12345, 'male', 01234567890, 3)
+
+INTO Student 
+(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+VALUES
+('ahmed', 'adel', '01-5-1999', 'ahadel@mail.com', 02140, 'male', 010345485870, 2)
+
+INTO Student 
+(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+VALUES
+('mahmoud', 'fady', '03-7-1999', 'mahmoud@mail.com', 12005, 'male', 01534111890, 3)
+
+INTO Student 
+(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+VALUES
+('amal', 'salama', '01-4-1999', 'amal@mail.com', 17745, 'female', 01034567878, 4)
+
+INTO Student 
+(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+VALUES
+('amany', 'hany', '01-6-1999', 'amanyss@mail.com', 13645, 'female', 01134567890, 3)
+
+SELECT * FROM dual;
