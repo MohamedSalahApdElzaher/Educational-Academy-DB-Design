49533ee994c31b065ed8f471308e6aec4a9d34d8
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2021-05-31 09:53:02 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,13 @@
-SELECT * FROM Student;
+SELECT * FROM Student
+WHERE gender = 'male' OR gender = 'female'
+ORDER BY levels;
+
+UPDATE Student
+SET date_of_birth = '01-2-1999'
+WHERE first_name = 'muhammad' AND last_name = 'salah';
+
+DELETE Student
+WHERE id = 3;
+
+
+
