4bad3bc514ce3982ab2191b604b38e9d971d933c
Mohamed-Abd-El-Nasser <47921242+Mohamed-Abd-El-Nasser@users.noreply.github.com>
2021-06-06 02:12:54 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,28 +1,167 @@
-INSERT All 
+
+
+INSERT all
+INTO Student 
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
+VALUES
+(1,'muhammad', 'salah', '01-02-1999', 'mosala7@gmail.com', 12345, 'male', 01234567890, 3);
 
 INTO Student 
-(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
 VALUES
-('muhammad', 'salah', '01-2-1999', 'mosala7@mail.com', 12345, 'male', 01234567890, 3)
+(2,'ahmed', 'adel', '01-5-1999', 'ahadel@mail.com', 02140, 'male', 010345485870, 2)
 
 INTO Student 
-(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
 VALUES
-('ahmed', 'adel', '01-5-1999', 'ahadel@mail.com', 02140, 'male', 010345485870, 2)
+(3,'mahmoud', 'fady', '03-7-1999', 'mahmoud@mail.com', 12005, 'male', 01534111890, 3)
 
 INTO Student 
-(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
 VALUES
-('mahmoud', 'fady', '03-7-1999', 'mahmoud@mail.com', 12005, 'male', 01534111890, 3)
+(4,'amal', 'salama', '01-4-1999', 'amal@mail.com', 17745, 'female', 01034567878, 4)
 
 INTO Student 
-(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
 VALUES
-('amal', 'salama', '01-4-1999', 'amal@mail.com', 17745, 'female', 01034567878, 4)
+(5,'amany', 'hany', '01-6-1999', 'amanyss@mail.com', 13645, 'female', 01134567890, 3)
 
 INTO Student 
-(first_name, last_name, date_of_birth, email, credit_card, gender, phone, st_level)
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
 VALUES
-('amany', 'hany', '01-6-1999', 'amanyss@mail.com', 13645, 'female', 01134567890, 3)
+(6,'moahmed', 'adel', '07-08-1999', 'moadel7@gmail.com', 13140, 'male', 01234567890, 3);
 
-SELECT * FROM dual;
+INTO Student 
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
+VALUES
+(7,'moahmed', 'shafie', '19-2-2000', 'abdm90486@mail.com', 60076, 'male', 010345485870, 2)
+
+INTO Student 
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
+VALUES
+(8,'youssef', 'magdy', '01-4-1999', 'youssef@mail.com', 89760, 'male', 01534111890, 1)
+
+INTO Student 
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
+VALUES
+(9,'farouk', 'ayman', '23-6-1999', 'ayman@mail.com', 13645, 'male', 01034567878, 1)
+
+INTO Student 
+(id,first_name, last_name, date_of_birth, email, credit_card, gender, phone, levels)
+VALUES
+(10,'sara', 'mohamed', '13-1-2000', 'sara@mail.com', 37645, 'female', 01134569890, 3)
+
+
+SELECT * FROM daul;
+
+
+
+insert all
+into address
+(user_id,street,city,country,student_id)
+values
+(1,'1th Saad Zagloul Street','shibin alqanatir','Egypt',1)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(2,'15th alharih Street','shibin alqanatir','Egypt',2)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(3,'24 aljumhuriih Street','shibin alqanatir','Egypt',3)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(4,'8th Saad Zagloul Street','shibin alqanatir','Egypt',4)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(5,'9th alamam alghazaliu Street','shibin alqanatir','Egypt',5)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(6,'17th alabtal Street','shibin tukh','Egypt',6)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(7,'29th Abd ElRahman bin Auf Street','alqanatir','Egypt',7)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(8,'52th Sudan Street','hilmiat alzaytun','Egypt',8)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(9,'31th Saad Zagloul Street','shibin alqanatir','Egypt',9)
+
+
+into address
+(user_id,street,city,country,student_id)
+values
+(10,'19th Ahmed Orabi Street','Banha','Egypt',10)
+
+select * from daul ;
+
+insert all 
+into departmente
+(department_id,department_name,department_details,manager)
+values 
+(10,'civil','The Department of Civil Engineering is committed to building an inclusive campus community with accessible services that respect the dignity and independence of persons with disabilities.' ,'dr eng/Rajab Mujahid')
+
+
+into departmente
+(department_id,department_name,department_details,manager)
+values 
+(20,'electrical','Electrical engineering is an engineering discipline concerned with the study, design and application of equipment, devices and systems which use electricity, electronics, and electromagnetism.' ,'dr eng/Sabah')
+
+
+
+into departmente
+(department_id,department_name,department_details,manager)
+values 
+(30,'mechanical','Mechanical engineering is an engineering branch that combines engineering physics and mathematics principles with materials science to design, analyze, manufacture, and maintain mechanical systems' ,'dr eng/Fouad Helmy')
+
+
+
+into departmente
+(department_id,department_name,department_details,manager)
+values 
+(40,'survey','Surveying or land surveying is the technique, profession, art, and science of determining the terrestrial or three-dimensional positions of points and the distances and angles between them.' ,'dr eng/ahmed')
+
+
+into departmente
+(department_id,department_name,department_details,manager)
+values 
+(50,'Architctural','architecture engineering, is an engineering discipline that deals with the technological aspects and multi-disciplinary approach to planning, design, construction and operation of buildings' ,'dr eng/mahmoud')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
